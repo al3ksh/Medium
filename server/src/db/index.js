a2b516aea447493d2e11e805ad1a4061c5d79bb5
Aleks Szotek <alex.szotek@gmail.com>
2026-04-26 16:44:47 +0200

    fix: docker nginx proxy + sqlite volume mount path

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3')
 const path = require('path')
 
-const DB_PATH = path.join(__dirname, '..', '..', 'medium.db')
+const DB_PATH = path.join(__dirname, '..', '..', 'data', 'medium.db')
 
 const db = new Database(DB_PATH)
 
